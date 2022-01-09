804391530d4c4c2690a5ce4f883cd214846d5712
unknown <wanted1345@gmail.com>
2022-01-09 22:46:03 +0900

    add custom test

---
+++ test/campus_chat/db/insert_query_test.exs
@@ -0,0 +1,10 @@
+defmodule CampusChat.InsertQueryTest do
+  use CampusChat.DataCase
+
+  alias CampusChat.Message
+
+  test "insert message" do
+    str = "There will be tests here someday"
+    assert str == "Ahahaha, No"
+  end
+end
