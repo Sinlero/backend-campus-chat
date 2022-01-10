1424f1345e0bef2809a2acf1dc2d02e1079ba0b8
goldfinch_p8a <wanted1345@gmail.com>
2022-01-10 16:11:29 +0900

    insert message query test

--- test/campus_chat/db/insert_query_test.exs
+++ test/campus_chat/db/insert_query_test.exs
@@ -4,7 +4,16 @@ defmodule CampusChat.InsertQueryTest do
   alias CampusChat.Message
 
   test "insert message" do
-    str = "There will be tests here someday"
-    assert str == "Ahahaha, No"
+    msg = %Message{
+      chat_id: 1,
+      date: NaiveDateTime.truncate(NaiveDateTime.utc_now(), :second),
+      sender_id: 1,
+      text: "Test Text",
+      test_field: "string"
+    }
+    case {_result, _struct} = Repo.insert(msg) do
+      {:ok, _struct}    -> assert true
+      {:error, _struct} -> assert false
+    end
   end
 end
