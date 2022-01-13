c85cabb1f4c77d70e8ff690e46ab4d3f8be25a63
Sinlero <wanted1345@gmail.com>
2022-01-13 21:11:15 +0900

    add read query test

--- test/campus_chat/db/insert_query_test.exs
+++ test/campus_chat/db/insert_query_test.exs
@@ -4,6 +4,18 @@ defmodule CampusChat.InsertQueryTest do
   alias CampusChat.Message
 
   test "insert message" do
+    case insertMessageQuery() do
+      {:ok, _struct}    -> assert true
+      {:error, _changeset} -> assert false
+    end
+  end
+
+  test "read message" do
+    {:ok, msg} = insertMessageQuery()
+    assert msg.id == Repo.get(Message, msg.id).id
+  end
+
+  defp insertMessageQuery() do
     msg = %Message{
       chat_id: 1,
       date: NaiveDateTime.truncate(NaiveDateTime.utc_now(), :second),
@@ -11,9 +23,7 @@ defmodule CampusChat.InsertQueryTest do
       text: "Test Text",
       test_field: "string"
     }
-    case Repo.insert(msg) do
-      {:ok, _struct}    -> assert true
-      {:error, _changeset} -> assert false
-    end
+    Repo.insert(msg)
   end
+
 end
