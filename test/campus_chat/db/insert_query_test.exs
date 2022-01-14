717785c198cda461980042572ae0537d5a360dd4
goldfinch_p8a <wanted1345@gmail.com>
2022-01-14 10:23:20 +0900

    refactor with describe block

--- test/campus_chat/db/insert_query_test.exs
+++ test/campus_chat/db/insert_query_test.exs
@@ -3,27 +3,31 @@ defmodule CampusChat.InsertQueryTest do
 
   alias CampusChat.Message
 
-  test "insert message" do
-    case insertMessageQuery() do
-      {:ok, _struct}    -> assert true
-      {:error, _changeset} -> assert false
+  describe "insert/read message" do
+
+    test "insert message" do
+      case insertMessageQuery() do
+        {:ok, _struct}    -> assert true
+        {:error, _changeset} -> assert false
+      end
     end
-  end
 
-  test "read message" do
-    {:ok, msg} = insertMessageQuery()
-    assert msg.id == Repo.get(Message, msg.id).id
-  end
+    test "read message" do
+      {:ok, msg} = insertMessageQuery()
+      assert msg.id == Repo.get(Message, msg.id).id
+    end
 
-  defp insertMessageQuery() do
-    msg = %Message{
-      chat_id: 1,
-      date: NaiveDateTime.truncate(NaiveDateTime.utc_now(), :second),
-      sender_id: 1,
-      text: "Test Text",
-      test_field: "string"
-    }
-    Repo.insert(msg)
+    defp insertMessageQuery() do
+      msg = %Message{
+        chat_id: 1,
+        date: NaiveDateTime.truncate(NaiveDateTime.utc_now(), :second),
+        sender_id: 1,
+        text: "Test Text",
+        test_field: "string"
+      }
+      Repo.insert(msg)
+    end
+
   end
 
 end
