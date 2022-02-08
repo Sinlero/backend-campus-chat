39e158966c85ac32a6f33e8c5d16457dfa7d8272
goldfinch_p8a <wanted1345@gmail.com>
2022-02-08 12:44:57 +0900

    add category schema

---
+++ lib/campus_chat/schemas/category.ex
@@ -0,0 +1,17 @@
+defmodule CampusChat.Category do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "category" do
+    field :name,      :string
+    field :type,      :string
+    field :remote_id, :integer
+    many_to_many :users, CampusChat.User, join_through: "users_categories"
+  end
+
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:name, :type, :remote_id])
+    |> validate_required([:name, :type])
+  end
+end
