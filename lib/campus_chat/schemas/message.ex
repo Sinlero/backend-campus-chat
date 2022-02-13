4ba657ec909322f52dc2d5ce0811935e93f8ba00
Sinlero <wanted1345@gmail.com>
2022-02-13 19:35:35 +0900

    add association with room

--- lib/campus_chat/schemas/message.ex
+++ lib/campus_chat/schemas/message.ex
@@ -3,11 +3,9 @@ defmodule CampusChat.Message do
   import Ecto.Changeset
 
   schema "messages" do
-    field :chat_id, :integer
-    field :date, :naive_datetime
     field :sender_id, :integer
+    belongs_to :room, CampusChat.Room
     field :text, :string
-    field :test_field, :string
 
     timestamps()
   end
@@ -15,7 +13,8 @@ defmodule CampusChat.Message do
   @doc false
   def changeset(message, attrs) do
     message
-    |> cast(attrs, [:sender_id, :text, :chat_id, :date, :test_field])
-    |> validate_required([:sender_id, :text, :chat_id, :date, :test_field])
+    |> cast(attrs, [:sender_id, :text])
+    |> validate_required([:sender_id, :text])
   end
+
 end
