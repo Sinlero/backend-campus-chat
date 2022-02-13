22fd6911f3565b93e7df10c5170e1c16ad95c564
Sinlero <wanted1345@gmail.com>
2022-02-13 19:27:33 +0900

    add users_tokens migration

---
+++ priv/repo/migrations/20220211072203_create_users_tokens.exs
@@ -0,0 +1,18 @@
+defmodule CampusChat.Repo.Migrations.CreateUsersTokens do
+  use Ecto.Migration
+
+  def change do
+
+    create table(:users_tokens) do
+      add :user_id, :integer
+      add :token, :binary, null: false
+      add :context, :string, null: false
+      add :sent_to, :string
+      timestamps(updated_at: false)
+    end
+
+    create index(:users_tokens, [:user_id])
+    create unique_index(:users_tokens, [:context, :token])
+
+  end
+end
