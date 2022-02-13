c2a696637f9263e29b2c6478e88ad8e5e738aa8d
Sinlero <wanted1345@gmail.com>
2022-02-13 19:26:20 +0900

    add reference to room and change type text field

--- priv/repo/migrations/20220107065235_create_messages.exs
+++ priv/repo/migrations/20220107065235_create_messages.exs
@@ -2,11 +2,10 @@ defmodule CampusChat.Repo.Migrations.CreateMessages do
   use Ecto.Migration
 
   def change do
-    create table(:messages) do
+    create table("messages") do
       add :sender_id, :integer
-      add :text, :string
-      add :chat_id, :integer
-      add :date, :naive_datetime
+      add :text, :text
+      add :room_id, references("rooms")
 
       timestamps()
     end
