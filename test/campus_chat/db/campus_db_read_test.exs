2bbafc62bf165f3910168d7640aa71dbf4ef6c68
Sinlero <wanted1345@gmail.com>
2022-02-13 19:40:42 +0900

    refactor tests

--- test/campus_chat/db/campus_db_read_test.exs
+++ test/campus_chat/db/campus_db_read_test.exs
@@ -1,10 +1,11 @@
 defmodule CampusChat.CampusDbReadTest do
   use CampusChat.DataCase
 
-  alias CampusChat.CampusRepo
-  alias CampusChat.User
-  alias CampusChat.Category
   alias CampusChat.CampusQuery
+
+  @category_id 8
+  @count_students_in_4A 16
+  @count_groups_in_FMF 22
 
   @valid_user %{
     course: 4,
@@ -18,53 +19,40 @@ defmodule CampusChat.CampusDbReadTest do
     surname: "Щеголь"
   }
 
-  test "read user from campus by id" do
-    id = 1652
-    assert CampusQuery.getUserById(id) == @valid_user
+  test "get user by id" do
+    assert CampusQuery.get_user_by_id(@valid_user.id).id == @valid_user.id
   end
 
-  test "read all users" do
-    assert [] != CampusQuery.getAllUsers()
+  test "get user by id with categories" do
+    assert CampusQuery.get_user_by_id_with_categories(@valid_user.id).categories != []
+  end
+
+  test "get all users" do
+    assert CampusQuery.get_all_users() != []
   end
 
   test "get all categories" do
-    assert [] != CampusQuery.getAllCategories()
+    assert CampusQuery.get_all_categories() != []
   end
 
-  test "get users on selected category" do
-    category_id = 8 # ФМФ
-    assert [] != CampusQuery.getUsersByCategory(category_id)
+  test "get category by id with users" do
+    assert CampusQuery.get_users_by_category(@category_id) != []
+  end
+
+  test "get users by selected group" do
+    result = CampusQuery.get_users_by_group(@category_id, @valid_user.course, @valid_user.group_name).users |> Enum.count()
+    assert result == @count_students_in_4A
   end
 
   test "get list of groups and courses by category" do
-    category_id = 8 # ФМФ
-    assert [] != CampusQuery.getListOfGroupsAndCourses(category_id)
+    result = CampusQuery.get_list_of_groups_and_courses(@category_id)
+    |> Enum.count()
+    assert result == @count_groups_in_FMF
   end
 
-  test "get users on selected group" do
-    category_id = 8 # ФМФ
-    course = 4
-    group = "А"
-    assert [] != CampusQuery.getUsersByGroup(category_id, course, group)
-  end
-
-  test "get user by id (with schema)" do
-    user = CampusRepo.get(User, @valid_user.id)
-    IO.inspect(user)
-  end
-
-  test "get all categories (with schema)" do
-    query = from category in Category,
-            select: category
-    assert [] != CampusRepo.all(query)
-    # |> IO.inspect()
-  end
-
-  test "get category by id with users (with schema)" do
-    category_id = 8
-    CampusRepo.get(Category, category_id)
-    |> CampusRepo.preload([:users])
-    |> IO.inspect()
+  test "get user by login and password" do
+    result = CampusQuery.get_user_by_login_and_password(@valid_user.login, @valid_user.password)
+    assert result.id == @valid_user.id
   end
 
 end
