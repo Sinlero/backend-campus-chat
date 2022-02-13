26e38bb067f8e9abec85c1506a4836d57874f043
Sinlero <wanted1345@gmail.com>
2022-02-13 19:37:25 +0900

    refactor with schemas structures

--- lib/campus_chat/repos/campus/campus_query.ex
+++ lib/campus_chat/repos/campus/campus_query.ex
@@ -3,74 +3,61 @@ defmodule CampusChat.CampusQuery do
   import Ecto.Query
   alias CampusChat.CampusRepo
   alias CampusChat.User
+  alias CampusChat.Category
 
-  @spec getUserById(integer) :: struct
-  def getUserById(id) do
-
-    query = from user in "users",
-              where: user.id == ^id,
-              select: {user.id, user.name, user.surname, user.patronymic,
-              user.login, user.password, user.description, user.course, user.group_name}
-
-    CampusRepo.one(query)
-    |> User.new()
+  def get_user_by_id(id) do
+    CampusRepo.get(User, id)
   end
 
-  @spec getAllUsers :: list(struct)
-  def getAllUsers() do
-    query = from user in "users",
-      select: {user.id, user.name, user.surname, user.patronymic,
-      user.login, user.password, user.description, user.course, user.group_name}
+  def get_user_by_login_and_password(login, password) do
+    from(user in User, where: user.login == ^login and user.password == ^password)
+    |> CampusRepo.one()
+  end
 
+  def get_user_by_id_with_categories(id) do
+    get_user_by_id(id)
+    |> CampusRepo.preload([:categories])
+  end
+
+  def get_all_users() do
+    query = from user in User,
+            select: user
     CampusRepo.all(query)
   end
 
-  @spec getAllCategories :: list(struct)
+  def get_category_by_id(category_id) do
+    CampusRepo.get(Category, category_id)
+  end
+
   @doc """
   ФМФ, ИФФ, Сотрудники и т.д.
   """
-  def getAllCategories() do
-    query = from category in "category",
-            select: {category.id, category.name, category.type}
-
+  def get_all_categories() do
+    query = from category in Category,
+            select: category
     CampusRepo.all(query)
   end
 
-  @spec getUsersByCategory(any) :: any
   @doc """
   select * from users
     join users_categories on users.id = users_categories.user_id
         join category  on users_categories.category_id = category.id
   where category_id = 8
   """
-  def getUsersByCategory(category_id) do
-    query =
-      from user in "users",
-        join: user_category in "users_categories", on: user.id == user_category.user_id,
-            join: category in "category", on: user_category.category_id == category.id,
-              where: category.id == ^category_id,
-                select: {category.name, user.id, user.name, user.surname, user.patronymic,
-                        user.login, user.password, user.description, user.course, user.group_name}
-    CampusRepo.all(query)
+  def get_users_by_category(category_id) do
+    get_category_by_id(category_id)
+    |> CampusRepo.preload(:users)
   end
 
-  @spec getUsersByGroup(integer, integer, String.t()) :: any
   @doc """
   select * from users
     join users_categories on users.id = users_categories.user_id
         join category  on users_categories.category_id = category.id
   where category_id = 8 and course = 4 and group_name = 'А'
   """
-  def getUsersByGroup(category_id, course, group) do
-    query =
-      from user in "users",
-        join: user_category in "users_categories", on: user.id == user_category.user_id,
-            join: category in "category", on: user_category.category_id == category.id,
-              where: category.id == ^category_id and user.course == ^course and user.group_name == ^group,
-                select: {category.name, user.id, user.name, user.surname, user.patronymic,
-                        user.login, user.password, user.description, user.course, user.group_name}
-
-    CampusRepo.all(query)
+  def get_users_by_group(category_id, course, group) do
+    CampusRepo.get(Category, category_id)
+    |> CampusRepo.preload([users: from(u in User, where: u.course == ^course and u.group_name == ^group)])
   end
 
   @doc """
@@ -82,16 +69,13 @@ defmodule CampusChat.CampusQuery do
   group by course, group_name
   order by group_name, course
   """
-  def getListOfGroupsAndCourses(category_id) do
-    query =
-      from user in "users",
-        join: user_category in "users_categories", on: user.id == user_category.user_id,
-          join: category in "category", on: user_category.category_id == category.id,
-            where: category.id == ^category_id ,
-              group_by: [user.course, user.group_name],
-                order_by: [user.group_name, user.course],
-                  select: {user.course, user.group_name, count(user.course)}
-
+  def get_list_of_groups_and_courses(category_id) do
+    query = from category in Category,
+            join: user in assoc(category, :users),
+            where: category.id == ^category_id,
+            group_by: [user.course, user.group_name],
+            order_by: [user.course, user.group_name],
+            select:   {user.course, user.group_name, count(user.course)}
     CampusRepo.all(query)
   end
 
