eb8a308ab3f5e6ed9811871df98ab795ada92f4a
goldfinch_p8a <wanted1345@gmail.com>
2022-02-17 13:01:42 +0900

    add using fixtures

--- test/campus_chat/db/campus_db_read_test.exs
+++ test/campus_chat/db/campus_db_read_test.exs
@@ -3,28 +3,14 @@ defmodule CampusChat.CampusDbReadTest do
 
   alias CampusChat.CampusQuery
 
-  @category_id 8
-  @count_students_in_4A 16
-  @count_groups_in_FMF 22
-
-  @valid_user %{
-    course: 4,
-    description: "ФМФ 4А",
-    group_name: "А",
-    id: 1652,
-    login: "login",
-    name: "Андрей",
-    password: "password",
-    patronymic: "Викторович",
-    surname: "Щеголь"
-  }
+  import CampusChat.Fixtures
 
   test "get user by id" do
-    assert CampusQuery.get_user_by_id(@valid_user.id).id == @valid_user.id
+    assert CampusQuery.get_user_by_id(valid_user().id).id == valid_user().id
   end
 
   test "get user by id with categories" do
-    assert CampusQuery.get_user_by_id_with_categories(@valid_user.id).categories != []
+    assert CampusQuery.get_user_by_id_with_categories(valid_user().id).categories != []
   end
 
   test "get all users" do
@@ -36,23 +22,23 @@ defmodule CampusChat.CampusDbReadTest do
   end
 
   test "get category by id with users" do
-    assert CampusQuery.get_users_by_category(@category_id) != []
+    assert CampusQuery.get_users_by_category(valid_category()) != []
   end
 
   test "get users by selected group" do
-    result = CampusQuery.get_users_by_group(@category_id, @valid_user.course, @valid_user.group_name).users |> Enum.count()
-    assert result == @count_students_in_4A
+    result = CampusQuery.get_users_by_group(valid_category(), valid_user().course, valid_user().group_name).users |> Enum.count()
+    assert result == valid_count_students_in_4A()
   end
 
   test "get list of groups and courses by category" do
-    result = CampusQuery.get_list_of_groups_and_courses(@category_id)
+    result = CampusQuery.get_list_of_groups_and_courses(valid_category())
     |> Enum.count()
-    assert result == @count_groups_in_FMF
+    assert result == valid_count_groups_in_FMF()
   end
 
   test "get user by login and password" do
-    result = CampusQuery.get_user_by_login_and_password(@valid_user.login, @valid_user.password)
-    assert result.id == @valid_user.id
+    result = CampusQuery.get_user_by_login_and_password(valid_user().login, valid_user().password)
+    assert result.id == valid_user().id
   end
 
 end
