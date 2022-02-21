bf3a49264c21ebae3fc3a26af00917d9129583a7
goldfinch_p8a <wanted1345@gmail.com>
2022-02-21 17:07:10 +0900

    add test for get all categories

---
+++ test/campus_chat_web/controllers/search_controller_test.exs
@@ -0,0 +1,16 @@
+defmodule CampusChatWeb.SearchControllerTest do
+  use CampusChatWeb.ConnCase
+
+  import CampusChat.Fixtures
+
+  alias CampusChatWeb.SearchController
+
+  def logined_session() do
+    post(build_conn(), "/api/login", [login: valid_user().login, password: valid_user().password])
+  end
+
+  test "get all categories" do
+    conn = logined_session() |> get("/api/categories")
+    assert conn.resp_body != nil
+  end
+end
