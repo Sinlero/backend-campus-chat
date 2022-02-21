5fae66bdc9e16acb2b053cd26c7f138435c3322b
goldfinch_p8a <wanted1345@gmail.com>
2022-02-21 15:47:22 +0900

    add time to logs

--- config/dev.exs
+++ config/dev.exs
@@ -58,7 +58,7 @@ config :campus_chat, CampusChatWeb.Endpoint,
 # different ports.
 
 # Do not include metadata nor timestamps in development logs
-config :logger, :console, format: "[$level] $message\n"
+config :logger, :console, format: "[$time] [$level] $message\n"
 
 # Set a higher stacktrace during development. Avoid configuring such
 # in production as building large stacktraces may be expensive.
