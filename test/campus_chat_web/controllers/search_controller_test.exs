4f4d4b4e185bc28dc375c6df3d98f9ededb7d0ca
Andrey <wanted1345@gmail.com>
2022-03-02 21:09:47 +0900

    refactoring with prefix api, add tests for getting users by: category, group and course; only category

--- test/campus_chat_web/controllers/search_controller_test.exs
+++ test/campus_chat_web/controllers/search_controller_test.exs
@@ -4,16 +4,34 @@ defmodule CampusChatWeb.SearchControllerTest do
   import CampusChat.Fixtures
 
   def logined_session() do
-    post(build_conn(), "/api/login", [login: valid_user().login, password: valid_user().password])
+    post(build_conn(), "#{api_prefix()}/login", [login: valid_user().login, password: valid_user().password])
   end
 
   test "get all categories" do
-    conn = logined_session() |> get("/api/categories")
+    conn = logined_session() |> get("#{api_prefix()}/categories")
     assert conn.resp_body != nil
   end
 
   test "get groups and courses by category" do
-    response = logined_session() |> get("/api/category/#{valid_category()}") |> json_response(200)
+    response = logined_session() |> get("#{api_prefix()}/category/#{valid_category()}") |> json_response(200)
     assert response == CampusChat.SearchService.get_groups(valid_category())
+  end
+
+  test "get users of group and course" do
+    response_count_students =
+      logined_session()
+      |> get("#{api_prefix()}/users/category/#{valid_category()}/course/#{valid_user().course}/group/#{valid_user().group_name}")
+      |> json_response(200)
+      |> Enum.count()
+    assert response_count_students == valid_count_students_in_4A()
+  end
+
+  test "get users of category" do
+    response_count_users =
+      logined_session()
+      |> get("#{api_prefix()}/users/category/#{mip_id()}")
+      |> json_response(200)
+      |> Enum.count()
+    assert response_count_users == valid_count_users_in_mip()
   end
 end
