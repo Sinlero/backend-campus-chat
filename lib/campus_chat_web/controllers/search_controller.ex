51db9992635c60b00d21626c3307e17c0b4d0c5f
Andrey <wanted1345@gmail.com>
2022-03-02 21:06:46 +0900

    add methods to get users by: category, course and group; only category

--- lib/campus_chat_web/controllers/search_controller.ex
+++ lib/campus_chat_web/controllers/search_controller.ex
@@ -11,4 +11,12 @@ defmodule CampusChatWeb.SearchController do
     json(conn, SearchService.get_groups(category_id))
   end
 
+  def users_of_category(conn, %{"id" => category_id}) do
+    json(conn, SearchService.get_users(category_id))
+  end
+
+  def users_of_group_and_course(conn, %{"id" => category_id, "course" => course, "group" => group}) do
+    json(conn, SearchService.get_users(category_id, course, group))
+  end
+
 end
