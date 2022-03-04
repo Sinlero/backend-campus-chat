e9262aacc5046b3a9e1148351d1a4beaed07e4d0
Andrey <wanted1345@gmail.com>
2022-03-04 10:43:41 +0900

    add swagger deps and compile swagger file

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule CampusChat.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:gettext] ++ Mix.compilers(),
+      compilers: [:phoenix, :gettext] ++ Mix.compilers() ++ [:phoenix_swagger],
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
@@ -43,7 +43,9 @@ defmodule CampusChat.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:cors_plug, "~> 2.0.3"}
+      {:cors_plug, "~> 2.0.3"},
+      {:phoenix_swagger, "~> 0.8"},
+      {:ex_json_schema, "~> 0.5"}
     ]
   end
 
