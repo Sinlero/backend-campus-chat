d190ea29bb5b7f6dcb0bf974d4029697b4474398
Andrey <wanted1345@gmail.com>
2022-03-12 14:18:06 +0900

    change create method to basic auth with header and add swagger defenitions

--- lib/campus_chat_web/controllers/session_controller.ex
+++ lib/campus_chat_web/controllers/session_controller.ex
@@ -1,18 +1,24 @@
 defmodule CampusChatWeb.SessionController do
   use CampusChatWeb, :controller
+  use PhoenixSwagger
 
   alias CampusChat.CampusQuery
   alias CampusChat.AuthenticationService
+  alias CampusChatWeb.SwaggerParameters
 
-  def create(conn, %{"login" => login, "password" => password}) do
-    user_params = %{login: login, password: password}
-    if user = CampusQuery.get_user_by_login_and_password(login, password) do
-      AuthenticationService.log_in_user(conn, user, user_params)
+  swagger_path :create do
+    post "/login"
+    description "Authorization"
+    parameter("Credentials", :body, :string, "Campus user login and password", required: true)
+  end
+
+  def create(conn, _params) do
+    with {login, password} <- Plug.BasicAuth.parse_basic_auth(conn),
+           %CampusChat.User{} = user <- CampusQuery.get_user_by_login_and_password(login, password) do
+          AuthenticationService.log_in_user(conn, user, %{login: login, password: password})
     else
-      send_resp(conn, 401, "UNAUTHORIZED")
-      |> halt()
+      _ -> conn |> send_resp(403, "Forbidden") |> halt()
     end
-
   end
 
   def echo_me(conn, _params) do
@@ -20,8 +26,20 @@ defmodule CampusChatWeb.SessionController do
     json(conn, %{answer: "Echo"})
   end
 
+  swagger_path :get_token do
+    get "/token"
+    description "Get user token for phoenix channels"
+    SwaggerParameters.authorization()
+  end
+
   def get_token(conn, _params) do
     text(conn, conn.assigns[:user_token])
+  end
+
+  swagger_path :delete do
+    PhoenixSwagger.Path.delete "/login"
+    description "Logout user"
+    SwaggerParameters.authorization()
   end
 
   def delete(conn, _params) do
