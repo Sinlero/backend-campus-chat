4105db897d4826a3e93441ab6298f17413d883cc
Andrey <wanted1345@gmail.com>
2022-04-07 18:40:46 +0900

    change type user_id to bigint and add indexes to user_id and room_id

--- priv/repo/migrations/20220210084822_create_users_rooms_roles.exs
+++ priv/repo/migrations/20220210084822_create_users_rooms_roles.exs
@@ -3,9 +3,13 @@ defmodule CampusChat.Repo.Migrations.CreateUsersRoomsRoles do
 
   def change do
     create table("users_rooms_roles") do
-      add :user_id, :integer
+      add :user_id, :bigint
       add :room_id, references("rooms")
       add :role_id, references("roles")
     end
+
+    create index(:users_rooms_roles, [:user_id])
+    create index(:users_rooms_roles, [:room_id])
+
   end
 end
