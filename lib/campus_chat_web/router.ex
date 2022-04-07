b63c3a82980462d16cf517149a9a94b82d3e11d2
Andrey <wanted1345@gmail.com>
2022-04-07 18:45:01 +0900

    change token context and name of socket asssign

--- lib/campus_chat_web/router.ex
+++ lib/campus_chat_web/router.ex
@@ -61,8 +61,8 @@ defmodule CampusChatWeb.Router do
   # Putting user token to connection assign
   defp put_user_token(conn, _) do
     if current_user = conn.assigns[:current_user] do
-      token = Phoenix.Token.sign(conn, "user socket", current_user.id)
-      assign(conn, :user_token, token)
+      token = Phoenix.Token.sign(CampusChatWeb.Endpoint, "user socket", current_user.id)
+      assign(conn, :socket_token, token)
     else
       conn
     end
