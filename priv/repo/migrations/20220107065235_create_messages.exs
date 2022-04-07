91b94226ce795ef9a9994d877ac5528d2e474bc7
Andrey <wanted1345@gmail.com>
2022-04-07 18:39:51 +0900

    chage sender_id type to bigint and add indexes to room_id and sender_id

--- priv/repo/migrations/20220107065235_create_messages.exs
+++ priv/repo/migrations/20220107065235_create_messages.exs
@@ -3,11 +3,15 @@ defmodule CampusChat.Repo.Migrations.CreateMessages do
 
   def change do
     create table("messages") do
-      add :sender_id, :integer
+      add :sender_id, :bigint
       add :text, :text
       add :room_id, references("rooms")
 
       timestamps()
     end
+
+    create index(:messages, [:sender_id])
+    create index(:messages, [:room_id])
+
   end
 end
