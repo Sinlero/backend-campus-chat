76f09a276d41259980582ed43f5e2a84dd00fc93
Sinlero <wanted1345@gmail.com>
2022-04-24 16:52:59 +0900

    add test for creating chat group and refactor create dialog test

--- test/campus_chat_web/controllers/chat_controller_test.exs
+++ test/campus_chat_web/controllers/chat_controller_test.exs
@@ -10,12 +10,28 @@ defmodule CampusChatWeb.ChatControllerTest do
 
   test "create dialog with two users" do
     resp = logined_session() |> post("#{api_prefix()}/chat/dialog", %{"first" => 1652, "second" => 1991}) |> json_response(200)
-    assert is_map(resp) == true
+    case resp do
+      %{"id" => _value} -> assert true
+      _ -> assert false
+    end
   end
 
   test "create dialog with unexisted users" do
     resp = logined_session() |> post("#{api_prefix()}/chat/dialog", %{"first" => -1, "second" => -2}) |> response(404)
     assert resp =~ "User does not exist"
+  end
+
+  test "create chat group" do
+    body = %{
+      "id" => valid_user().id,
+      "users" => valid_ids_for_chat(),
+      "name" => "Test chat group name"
+    }
+    resp = logined_session() |> post("#{api_prefix()}/chat/group", body) |> json_response(200)
+    case resp do
+      %{"id" => _value} -> assert true
+      _ -> assert false
+    end
   end
 
 end
